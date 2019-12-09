d55b0f1c7400992304693305a1cfd4419e9550c9
Jason Doo <jeong7496@gmail.com>
2019-12-09 00:24:45 -0800

    day09 phew

--- day09/day09.py
+++ day09/day09.py
@@ -1,134 +1,84 @@
-with open('input.txt', 'r') as file:
-    data = file.read().split(',')
+memory = list(map(int,open('input.txt', 'r').read().split(',')))
 
-data = list(map(int, data))
+memory.extend([0] * 10000)
 
-# text might need more space
-# TODO
-data.extend([0]*10000)
-print(data)
+def val(mode, index, offset):
+  if mode == 0: # position mode
+    return memory[memory[index]]
+  elif mode == 1: # immediate mode
+    return memory[index]
+  elif mode == 2:
+    return memory[memory[index] + offset]
 
-def intcode():
-    rel_base = 0
-    i = 0
-    inpoot = 1
+def intcode(inp): 
+  offset, i = 0, 0
 
-    while i < len(data):
-        # print(rel_base)
-        inst = str(data[i]) 
-        # pad with zeros to make 5 characters
-        inst = list('0'* (5 - len(inst)) + inst)
-        DE = int(''.join(inst[-2:]))
+  while i < len(memory):
+    inst = str(memory[i]) 
+    inst = list('0'* (5 - len(inst)) + inst)
 
-        # for A,B,C, 1 is immediate mode, 0 is position mode
-        if DE == 1:
-            C, B, A = int(inst[-3]), int(inst[-4]), int(inst[-5])
-            p1, p2, p3 = data[i+1], data[i+2], data[i+3]
+    opcode = int(''.join(inst[-2:]))  # opcode
+    modes = list(map(int,inst[0:3]))  # parameter modes
+                                      # modes[0] = A
+                                      # modes[1] = B
+                                      # modes[2] = C
+    
+    # Parameters that an instruction writes to will never be in immediate mode
 
-            # new
-            p1, p2 = p1 + rel_base if C == 2 else p1, p2 + rel_base if B == 2 else p2
+    if opcode == 1: # add
+      p1 = val(modes[2], i+1, offset)
+      p2 = val(modes[1], i+2, offset)
+      p3 = memory[i+3] + (offset if modes[0] == 2 else 0)
+      memory[p3] = p1 + p2
+      i += 4
+    elif opcode == 2: # multiply
+      p1 = val(modes[2], i+1, offset)
+      p2 = val(modes[1], i+2, offset)
+      p3 = memory[i+3] + (offset if modes[0] == 2 else 0)
+      memory[p3] = p1 * p2
+      i += 4
+    elif opcode == 3: # get input
+      p1 = memory[i+1] + (offset if modes[2] == 2 else 0)
+      memory[p1] = inp
+      i += 2
+    elif opcode == 4: # print output
+      p1 = memory[i+1] + (offset if modes[2] == 2 else 0)
+      if modes[2] == 1:
+        print(p1)
+      else:
+        print(memory[p1])
+      i += 2
+    elif opcode == 5: # jump if true
+      p1 = val(modes[2], i+1, offset)
+      p2 = val(modes[1], i+2, offset)
+      i = p2 if p1 != 0 else i + 3
+    elif opcode == 6: # jump if false
+      p1 = val(modes[2], i+1, offset)
+      p2 = val(modes[1], i+2, offset)
+      i = p2 if p1 == 0 else i + 3
+    elif opcode == 7: # less than
+      p1 = val(modes[2], i+1, offset)
+      p2 = val(modes[1], i+2, offset)
+      p3 = memory[i+3] + (offset if modes[0] == 2 else 0)
+      memory[p3] = 1 if p1 < p2 else 0
+      i += 4
+    elif opcode == 8: # equal to
+      p1 = val(modes[2], i+1, offset)
+      p2 = val(modes[1], i+2, offset)
+      p3 = memory[i+3] + (offset if modes[0] == 2 else 0)
+      memory[p3] = 1 if p1 == p2 else 0
+      i += 4
+    elif opcode == 9: # adjust offset
+      p1 = val(modes[2], i+1, offset)
+      offset += p1
+      i += 2
+    elif opcode == 99:
+      return
+    else:
+      print('opcode does not exist')
+      return
 
-            data[p3] = (p1 if C == 1 else data[p1]) + (p2 if B == 1 else data[p2])
-            i += 4
-        elif DE == 2:
-            C, B, A = int(inst[-3]), int(inst[-4]), int(inst[-5])
-            p1, p2, p3 = data[i+1], data[i+2], data[i+3]
+# intcode(1) # PART 1
+intcode(2) # PART 2
 
-            # new
-            p1, p2 = p1 + rel_base if C == 2 else p1, p2 + rel_base if B == 2 else p2
 
-            data[p3] = (p1 if C == 1 else data[p1]) * (p2 if B == 1 else data[p2])
-            i += 4
-        elif DE == 3:
-            C = int(inst[-3])
-            p1 = data[i+1]
-
-            # support parameters
-            p1 = p1 + rel_base if C == 2 else p1
-
-            if C == 1:
-                data[p1] = inpoot
-            else:
-                data[data[p1]] = inpoot
-            i += 2
-        elif DE == 4:
-            C = int(inst[-3])
-            p1 = data[i+1]
-
-            # support parameters
-            p1 = p1 + rel_base if C == 2 else p1
-
-            if C == 1:
-                print(p1)
-            else:
-                print(data[p1])
-            i += 2
-        elif DE == 5:
-            C, B = int(inst[-3]), int(inst[-4])
-            p1, p2 = data[i+1], data[i+2]
-
-            # new
-            p1, p2 = p1 + rel_base if C == 2 else p1, p2 + rel_base if B == 2 else p2
-
-            p1, p2 = p1 if C == 1 else data[p1], p2 if B == 1 else data[p2]
-            if p1 != 0:
-                i = p2
-            else:
-                i += 3
-        elif DE == 6:
-            C, B = int(inst[-3]), int(inst[-4])
-            p1, p2 = data[i+1], data[i+2]
-
-            # new
-            p1, p2 = p1 + rel_base if C == 2 else p1, p2 + rel_base if B == 2 else p2
-
-            p1, p2 = p1 if C == 1 else data[p1], p2 if B == 1 else data[p2]
-            if p1 == 0:
-                i = p2
-            else:
-                i += 3
-        elif DE == 7:
-            C, B, A = int(inst[-3]), int(inst[-4]), int(inst[-5])
-            p1, p2, p3 = data[i+1], data[i+2], data[i+3]
-
-            # new
-            p1, p2 = p1 + rel_base if C == 2 else p1, p2 + rel_base if B == 2 else p2
-
-            p1, p2 = p1 if C == 1 else data[p1], p2 if B == 1 else data[p2]
-            if p1 < p2:
-                data[p3] = 1
-            else:
-                data[p3] = 0
-            i += 4
-        elif DE == 8:
-            C, B, A = int(inst[-3]), int(inst[-4]), int(inst[-5])
-            p1, p2, p3 = data[i+1], data[i+2], data[i+3]
-
-            # new
-            p1, p2 = p1 + rel_base if C == 2 else p1, p2 + rel_base if B == 2 else p2
-
-            p1, p2 = p1 if C == 1 else data[p1], p2 if B == 1 else data[p2]
-            if p1 == p2:
-                data[p3] = 1
-            else:
-                data[p3] = 0
-            i += 4
-        elif DE == 9:
-            C = int(inst[-3])
-            p1 = data[i+1]
-
-            if C == 1:
-                rel_base += p1
-            elif C == 0:
-                rel_base += data[p1]
-            else:
-                rel_base += data[p1 + rel_base]
-            i += 2
-        elif DE == 99:
-            break
-        else:
-            print("Wrong op code: ", DE)
-
-    return
-
-intcode()
