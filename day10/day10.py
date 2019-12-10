1a02c0034cc989d9ccd8290059c16eb787c62c8b
Jason Doo <jeong7496@gmail.com>
2019-12-09 23:48:10 -0800

    Day10

--- day10/day10.py
+++ day10/day10.py
@@ -3,11 +3,6 @@ import collections
 
 data = open('input.txt', 'r').read().split('\n')
 data = [list(r) for r in data]
-
-# print(data)
-
-# (x,y) -> data[y][x]
-
 aesteroid = [] # coordinates of all the aesteroids
 
 for r in range(len(data)):
@@ -23,48 +18,85 @@ ans = float('-inf')
 best_a = None
 for a1 in aesteroid:
   mapp = data.copy()
-  distances = []
-  for a2 in aesteroid:
-    dx, dy = abs(a1[0] - a2[0]), abs(a1[1] - a2[1])
-    c = math.sqrt(dx*dx + dy*dy)
-    distances.append((a2, c)) # coords, distance tuple
-  
-  # go through aesteroids in sorted order
-  distances.sort(key=lambda x: x[1])
   can_see = 0
-  distances.pop(0) # first aesteroid is itself
-  visited = collections.defaultdict(bool)
+  visited = set()
+  ax, ay = a1[0], a1[1]
+  # print('curr aesteroid', a1)
 
-  print('curr aesteroid', a1)
+  for a2 in aesteroid:
+    if a1 == a2:
+      continue
 
-  for a2, _ in distances:
-    if not visited[a2]:
-      # print(a2)
+    dx, dy = a2[0] - a1[0], a2[1] - a1[1]
+    gcd = math.gcd(dx, dy)
+    dx /= gcd
+    dy /= gcd
+
+    if (dx,dy) not in visited:
       can_see += 1
-
-      dx, dy = a2[0] - a1[0], a2[1] - a1[1]
-      if dx == 0:
-        cx, cy = a2[0], a2[1]
-        while 0 <= cx < num_cols and 0 <= cy < num_rows:
-          visited[(cx,cy)] = True
-          cy += -1 if dy < 0 else 1
-      elif dy == 0:
-        cx, cy = a2[0], a2[1]
-        while 0 <= cx < num_cols and 0 <= cy < num_rows:
-          visited[(cx,cy)] = True
-          cx += -1 if dx < 0 else 1
-      else:
-        cx, cy = a2[0], a2[1]
-        while 0 <= cx < num_cols and 0 <= cy < num_rows:
-          visited[(cx,cy)] = True
-          cx += dx
-          cy += dy
-  print(can_see)
+      visited.add((dx,dy))
 
   if can_see > ans:
     ans = can_see
     best_a = a1
 
-
-print('ans', ans)
 print(best_a)
+
+
+
+
+# PART 2
+mx, my = best_a[0], best_a[1]
+
+# compute all angles and sort from up and clockwise
+angles = []
+for a in aesteroid:
+  if a == monitor:
+    continue
+  
+  dx, dy = a[0] - mx, a[1] - my
+  dy *= -1
+  angle = math.atan2(dy,dx)
+  if angle > math.pi / 2.0:
+      angle -= 2 * math.pi
+  angles.append((angle,a))
+
+angles.sort(key=lambda x: x[0], reverse=True)
+
+# rotate and vaporize
+curr = 1
+aesteroid_num = 200
+seen = set() # seen angles
+
+for angle, a in angles:
+  if angle in seen:
+    continue
+
+  if curr == aesteroid_num:
+    # get all aetroids with this angle, find the closest one
+    p = []
+    # print(angles)
+    for xx, yy in angles:
+      if xx == angle:
+        p.append(yy)
+
+    dist = []
+    for ast in p: 
+      dx, dy = abs(ast[0] - mx), abs(ast[1] - my)
+      dist.append((ast, math.sqrt(dx*dx + dy*dy)))
+    dist.sort(key=lambda x : x[1])
+    print(f'vaporized aesteroid # {curr} at {dist[0][0]} at angle {angle}')
+    break
+  else:
+    print(f'vaporized aesteroid # {curr} at angle {angle}')
+    seen.add(angle)
+    curr += 1
+
+
+
+
+
+
+
+  
+  
