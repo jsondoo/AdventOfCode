db9f397fbcc5941602149998d0a6c712afb5963e
Jason Doo <jdoo@alumni.ubc.ca>
2019-12-16 00:39:11 -0800

    day16 some next level stuff

--- day16/day16.py
+++ day16/day16.py
@@ -1,5 +1,6 @@
 import collections
 thing = open('input.txt', 'r').read()
+offset = int(thing[:7]) # for part 2
 input_list = [int(x) for x in list(thing)]
 
 def get_pattern(pos, n): # 1-based position, length of pattern list
@@ -34,9 +35,28 @@ inp = input_list
 for i in range(num_phases):
   inp = run_phase(inp)
 
-print(inp[:8])
+print(f'Part 1: {inp[:8]}') # PART 1
 
 
+def run_phase2(inp):
+  total = 0
+  out = []
+  for i in range(len(inp)-1,-1,-1):
+    total += inp[i]
+    out.append(abs(total) % 10)
+
+  out.reverse()
+  return out
+
+# PART 2
+inp2 = input_list * 10000
+inp = inp2[offset:]
+n = len(inp)
+
+num_phases = 100
+for i in range(num_phases):
+  inp = run_phase2(inp)
+print(f'Part 2: {inp[:8]}') # PART 2
 
 
 
