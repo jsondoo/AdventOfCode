24d49983e1767b0349c38d93e355c5658ba6bd54
Jason Doo <jeong7496@gmail.com>
2020-12-13 22:38:37 -0800

    update

--- 2020/d13.py
+++ 2020/d13.py
@@ -3,3 +3,54 @@ import re
 
 f = open('./d13.txt', 'r')
 rows = f.read().split("\n")
+inst = rows[1].split(',')
+
+bus_ids = list(filter(lambda x: x != 'x', inst))
+bus_ids = list(map(lambda x: int(x), bus_ids))
+print(bus_ids)
+
+offsets = []
+
+prev_idx = None
+for idx, ch in enumerate(inst):
+  if ch != 'x':
+    offsets.append(-idx)
+print(offsets)
+
+# Part 2: used chinese remainder theorem
+# system of congruences:
+# x = -0  (mod 19)
+# x = -13 (mod 37)
+# x = -19 (mod 523)
+# x = -37 (mod 13)
+# x = -42 (mod 23)
+# x = -48 (mod 29)
+# x = -50 (mod 547)
+# x = -60 (mod 41)
+# x = -67 (mod 17)
+
+
+
+from functools import reduce
+def chinese_remainder(n, a):
+    sum = 0
+    prod = reduce(lambda a, b: a*b, n)
+    for n_i, a_i in zip(n, a):
+        p = prod // n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
+ 
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1: return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a%b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0: x1 += b0
+    return x1
+
+print(offsets)
+print(bus_ids)
+print(chinese_remainder(bus_ids, offsets))
--- 2020/d14.py
+++ 2020/d14.py
@@ -3,3 +3,39 @@ import re
 
 f = open('./d14.txt', 'r')
 rows = f.read().split("\n")
+
+memmap = defaultdict(int)
+
+curr_mask = 0
+for instruction in rows:
+  parts = instruction.split()
+  if parts[0] == 'mask':
+    curr_mask = parts[2]
+  else:
+    val = int(parts[2]) 
+    mem, address = parts[0].split("[")
+    address = int(address.replace("]",""))
+    bin_address = format(address, 'b').zfill(36)
+
+    new_address = ""
+    for idx, ch in enumerate(curr_mask):
+      if ch == '0':
+        new_address += bin_address[idx]
+      elif ch == '1':
+        new_address += "1"
+      elif ch == 'X':
+        new_address += "X"
+    
+    def write_to_address(addr):
+      if addr.count('X') == 0:
+        memmap[addr] = val
+
+      for idx, ch in enumerate(addr):
+        if ch == 'X':
+          write_to_address(addr[:idx] + "1" + addr[idx+1:])
+          write_to_address(addr[:idx] + "0" + addr[idx+1:])
+          return
+
+    write_to_address(new_address)
+                                                                                                                                          
+print(sum(memmap.values()))
