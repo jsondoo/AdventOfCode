5e9a3cd9758e102ba1ae565378e4cd37194aa66d
Jason Doo <jeong7496@gmail.com>
2020-12-15 23:08:27 -0800

    oof

--- 2020/d15.py
+++ 2020/d15.py
@@ -1,5 +1,26 @@
 from collections import defaultdict
 import re
 
-f = open('./d15.txt', 'r')
-rows = f.read().split("\n")
+# start = [17,1,3,16,19,0]
+start = [17,1,3,16,19,0]
+
+seen = defaultdict(list) # which turns the number was seen 0:[1,2,3]
+counts = defaultdict(int)
+for idx, num in enumerate(start):
+  seen[num].append(idx+1)
+  counts[num] += 1
+
+turn = len(start) + 1
+last_number = start[-1]
+
+while turn <= 30000000:
+  if counts[last_number] == 1:
+    last_number = 0
+  else:
+    last_number = seen[last_number][-1] - seen[last_number][-2]
+
+  counts[last_number] += 1
+  seen[last_number].append(turn)
+  turn += 1
+
+print(last_number)
--- 2020/d16.py
+++ 2020/d16.py
@@ -2,4 +2,82 @@ from collections import defaultdict
 import re
 
 f = open('./d16.txt', 'r')
-rows = f.read().split("\n")
+rows = f.read().split("\n\n")
+
+range_list = []
+field_to_ranges = defaultdict(list)
+parts = rows[0].split("\n")
+for p in parts:
+  numbers = p.split(':')
+  ranges = numbers[1].strip().split("or")
+  for r in ranges:
+    num1, num2 = r.split("-")
+    tup = (int(num1),int(num2))
+    field_to_ranges[numbers[0]].append(tup)
+    range_list.append(tup)
+
+def check_valid(value):
+  for r in range_list:
+    if r[0] <= value <= r[1]:
+      return True
+  return False
+
+valid_tickets = []
+
+tickets = rows[-1].split('\n')[1:]
+
+for ticket in tickets:
+  valid = True
+  values = ticket.split(',')
+  for value in values:
+    value = int(value)
+    if not check_valid(value):
+      valid = False
+      break
+  
+  if valid:
+    valid_tickets.append(ticket)
+
+print(valid_tickets)
+columns = list(zip(*[[int(x) for x in valid_ticket.split(',')] for valid_ticket in valid_tickets]))
+
+possibles = []
+for column in columns:
+  possible = set(field_to_ranges.keys())
+
+  for value in column:
+    for field, ranges in field_to_ranges.items():
+      # see if value works for current ranges
+      rangeA, rangeB = ranges[0], ranges[1]
+      if rangeA[0] <= value <= rangeA[1] or rangeB[0] <= value <= rangeB[1]:
+        continue
+      elif field in possible:
+        possible.remove(field)
+
+  possibles.append(possible)
+
+ans = {}
+while True:
+  if len(ans) == 20:
+    break
+  for idx, p in enumerate(possibles):
+    if len(p) == 1:
+      field = list(p)[0]
+      ans[idx] = field
+      for idx, p in enumerate(possibles):
+        if len(p) > 0:
+          p.remove(field)
+          possibles[idx] = p
+      break
+
+indexes = []
+for k,v in ans.items():
+  if "departure" in v:
+    indexes.append(k)
+
+total = 1
+_, my_ticket = rows[1].split('\n')
+vals = [int(x) for x in my_ticket.split(',')]
+for idx in indexes:
+  total *= vals[idx]
+print(total)
