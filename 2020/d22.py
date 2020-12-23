8fe785b9aef5bae693def4d8e556d1c4ddbcd0f9
Jason Doo <jeong7496@gmail.com>
2020-12-23 01:43:11 -0800

    catch up on days

--- 2020/d22.py
+++ 2020/d22.py
@@ -2,4 +2,67 @@ from collections import defaultdict
 import re
 
 f = open('./d22.txt', 'r')
-rows = f.read().split("\n")
+player1, player2 = f.read().split("\n\n")
+
+player1 = [int(x) for x in player1.split("\n")[1:]]
+player2 = [int(x) for x in player2.split("\n")[1:]]
+
+assert(len(player1) == len(player2))
+
+game_states = defaultdict(set) # game_id -> seen states
+next_game_id = 0
+
+def simulate_game(player1, player2, game_id=0):
+  global next_game_id
+  next_game_id += 1
+  while player1 and player2:
+    # check if this game state existed (if so player1 immediately wins)
+    game_state = ''.join(str(v) for v in player1) + ":" + ''.join(str(v) for v in player2)
+    if game_state in game_states[game_id]:
+      return (0, True)
+
+    # otherwise, save game state and continue
+    game_states[game_id].add(game_state)
+
+    p1_top, p2_top = player1.pop(0), player2.pop(0)
+
+    # check if subgame needs to be played
+    subgame = False
+    if len(player1) >= p1_top and len(player2) >= p2_top:
+      _, p1_winner = simulate_game(player1[:p1_top], player2[:p2_top], next_game_id)
+      
+      if p1_winner:
+        player1.append(p1_top)
+        player1.append(p2_top)
+      else:
+        player2.append(p2_top)
+        player2.append(p1_top)
+
+    else:
+      # else continue with normal rules
+      if p1_top > p2_top:
+        player1.append(p1_top)
+        player1.append(p2_top)
+      else:
+        player2.append(p2_top)
+        player2.append(p1_top)
+
+  def compute_winning_score(lst):
+    total_score = 0
+    multipler = 1
+    for val in lst[::-1]:
+      total_score += val * multipler
+      multipler += 1
+    return total_score
+
+  part1 = None
+  player1_won = True
+  if player1:
+    part1 = compute_winning_score(player1)
+  else:
+    player1_won = False
+    part1 = compute_winning_score(player2)
+
+  return (part1, player1_won)
+
+print(simulate_game(player1, player2))
--- 2020/d23.py
+++ 2020/d23.py
@@ -1,5 +1,92 @@
-from collections import defaultdict
-import re
+input_string = "952316487"
+num_moves = 10000000
 
-f = open('./d23.txt', 'r')
-rows = f.read().split("\n")
+# PART 1 using circular linked list
+class Cup:
+  def __init__(self, val, next=None):
+    self.val = val
+    self.next = next
+
+  def print(self):
+    print(val)
+
+class CircularCups:
+  def __init__(self):
+    self.curr = None
+    self.val_to_cup = {}
+    self.tail = None
+
+  def push(self, val):
+    cup = Cup(val)
+    self.val_to_cup[val] = cup
+    if self.curr is None:
+      self.curr = cup
+      self.curr.next = self.curr
+      self.tail = self.curr
+    else:
+      self.tail.next = cup
+      cup.next = self.curr
+      self.tail = cup
+
+  def print(self):
+    temp = self.curr
+    if temp is not None:
+      formatted_string = str(temp.val)
+      while temp.next != self.curr:
+        formatted_string += " -> " + str(temp.next.val)
+        temp = temp.next
+      print(formatted_string)
+    
+
+cups = CircularCups()
+for ch in input_string:
+  cups.push(int(ch))
+
+for i in range(10, 1000000+1):
+  cups.push(i)
+
+for move in range(num_moves):
+  if move % 1000000 == 0:
+    print(move)
+  # start from current cup 
+  current_cup = cups.curr
+
+  # get the value of the next three cups
+  three_cups_values = set()
+  temp = current_cup.next
+  for i in range(3):
+    three_cups_values.add(temp.val)
+    temp = temp.next
+
+  # temporarily remove the next three cups
+  three_cups = current_cup.next
+  current_cup.next = current_cup.next.next.next.next
+
+  # find the label
+  next_cup_val = current_cup.val - 1
+
+  while next_cup_val == 0 or next_cup_val in three_cups_values:
+    next_cup_val -= 1
+    if next_cup_val <= 0:
+      next_cup_val = 1000000
+
+  # insert at label
+  label_cup = cups.val_to_cup[next_cup_val]
+  label_cup_next = label_cup.next
+  label_cup.next = three_cups
+  three_cups.next.next.next = label_cup_next
+
+  # update new current cup
+  cups.curr = cups.curr.next
+
+
+cup_1 = cups.val_to_cup[1]
+# temp = cup_1.next
+# part_1 = ""
+# while temp.val != 1:
+#   part_1 += str(temp.val)
+#   temp = temp.next
+# print(part_1)
+
+part_2 = cup_1.next.val * cup_1.next.next.val
+print(part_2)
